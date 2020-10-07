4a3a5f838a493ded1c295568445d1b3fb5717542
gonsaje <song.jaehyun@gmail.com>
2020-10-07 18:51:34 -0400

    added list-partion algo

--- algos_practice.js
+++ algos_practice.js
@@ -335,3 +335,27 @@ class Solution {
         return revStr === str;
     }
 }
+
+// List Partitioning
+// Given a list of strings strs, containing the strings "red", "green" and "blue", partition the list so that the red come before green, which come before blue.
+
+// Constraints
+
+// n ≤ 100,000 where n is the length of strs.
+// This should be done in \mathcal{O}(n)O(n) time.
+
+// Bonus: Can you do it in \mathcal{O}(1)O(1) space? That is, can you do it by only rearranging the list (i.e. without creating any new strings)?
+
+// Example 1
+// Input
+
+// strs = ["green", "blue", "red", "red"]
+// Output
+
+// ["red", "red", "green", "blue"]
+
+class Solution {
+    solve(strs) {
+        
+    }
+}
