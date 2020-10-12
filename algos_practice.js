a9e770e86c2c9fb230b66172ecdbe6f9ff07c528
gonsaje <song.jaehyun@gmail.com>
2020-10-12 16:32:36 -0400

    Majority Vote

--- algos_practice.js
+++ algos_practice.js
@@ -359,3 +359,40 @@ class Solution {
         
     }
 }
+
+// Majority Vote
+// You are given a list of integers nums containing n integers, where each number represents a vote to a candidate.
+
+// Return the id of the candidate that has \gt \lfloor \frac{n}{2}\rfloor>⌊ 
+// 2
+// n
+// ​	 ⌋ votes. If there's not a majority vote, return -1.
+
+// This should be done in \mathcal{O}(1)O(1) space.
+
+// Constraints
+
+// n ≤ 100,000
+// Example 1
+// Input
+
+// nums = [5, 5, 1, 1, 2, 2, 2, 2, 2]
+// Output
+
+// 2
+// Example 2
+// Input
+
+// nums = [3, 3, 4, 4]
+// Output
+
+// -1
+// Explanation
+
+// Neither 3 or 4 have more than 2 votes.
+
+class Solution {
+    solve(nums) {
+        
+    }
+}
