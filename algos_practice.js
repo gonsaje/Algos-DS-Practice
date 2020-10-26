6c5520ec8f55c5de5e26cba98b3d025fb587e04d
gonsaje <song.jaehyun@gmail.com>
2020-10-26 13:26:18 -0400

    root

--- algos_practice.js
+++ algos_practice.js
@@ -535,3 +535,36 @@ Difficulty: Hard
 function pyramidScheme(base) {
 
 }
+
+// Guess the root
+// Given a non-negative integer n, find a number r such that r * r = n and round down to the nearest integer.
+
+// Can you implement this without using the built-in square root?
+
+// Example 1
+// Input
+
+// n = 9
+// Output
+
+// 3
+// Explanation
+
+// 3 is the square root of 9.
+
+// Example 2
+// Input
+
+// n = 26
+// Output
+
+// 5
+// Explanation
+
+// ~5.09901951359 is the square root of 26 and rounding down gives us 5.
+
+class Solution {
+    solve(n) {
+        return Math.floor(Math.sqrt(n));
+    }
+}
